5ce10be046906a8cae2972aed84174db143d63bb
unknown <himanshu.p19@iiits.in>
2021-09-27 19:10:59 +0530

    stripe API integrated

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
@@ -9,8 +9,22 @@ const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
 
 const express = require("express");
 const app = express();
+const fs = require('fs');
 
 app.set("view engine", "ejs");
 app.use(express.static("public")); // Static() tells the app to make all the files inside the public folder static so that they can be used for frontend work.
+
+app.get('/store', function (req, res) {
+    fs.readFile('items.json', function (err, data) {
+        if (err) {
+            res.status(500).end();
+        } else {
+            res.render('store.ejs', {
+                stripePublicKey: stripePublicKey,
+                items: JSON.parse(data)
+            })
+        }
+    })
+});
 
 app.listen(3000);
